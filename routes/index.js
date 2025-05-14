b1f1d0a7dfa2da6c420072a8244c8e9cfcbb480e
moes99 <mohammadomarshehab@gmail.com>
2025-05-14 17:52:43 +0300

    added feedback table in sql, feedback routes, and feedback controller.

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Route modules
-const userRoutes = require('./userRoutes');
-const eventRoutes = require('./eventRoutes');
-const individualRoutes = require('./individualRoutes');
-const adminRoutes = require('./adminRoutes');
-const companyRoutes = require('./companyRoutes');
-const searchRoutes = require('./searchRoutes');
-const bookingRoutes = require('./bookingRoutes');
-const paymentRoutes = require('./paymentRoutes');
-const reviewRoutes = require('./reviewRoutes');
-const adRoutes = require('./adRoutes');
-const recommendationRoutes = require('./recommendationRoutes');
+const userRoutes = require("./userRoutes");
+const eventRoutes = require("./eventRoutes");
+const individualRoutes = require("./individualRoutes");
+const adminRoutes = require("./adminRoutes");
+const companyRoutes = require("./companyRoutes");
+const searchRoutes = require("./searchRoutes");
+const bookingRoutes = require("./bookingRoutes");
+const paymentRoutes = require("./paymentRoutes");
+const reviewRoutes = require("./reviewRoutes");
+const adRoutes = require("./adRoutes");
+const recommendationRoutes = require("./recommendationRoutes");
+const feedbackRoutes = require("./feedbackRoutes");
 
 // Use routes with appropriate base paths
-router.use('/users', userRoutes);
-router.use('/events', eventRoutes);
-router.use('/individual', individualRoutes);
-router.use('/admin', adminRoutes);
-router.use('/company', companyRoutes);
-router.use('/search', searchRoutes);
-router.use('/bookings', bookingRoutes);
-router.use('/payments', paymentRoutes);
-router.use('/reviews', reviewRoutes);
-router.use('/ads', adRoutes);
-router.use('/recommendations', recommendationRoutes);
+router.use("/users", userRoutes);
+router.use("/events", eventRoutes);
+router.use("/individual", individualRoutes);
+router.use("/admin", adminRoutes);
+router.use("/company", companyRoutes);
+router.use("/search", searchRoutes);
+router.use("/bookings", bookingRoutes);
+router.use("/payments", paymentRoutes);
+router.use("/reviews", reviewRoutes);
+router.use("/ads", adRoutes);
+router.use("/recommendations", recommendationRoutes);
+router.use("/feedback", feedbackRoutes);
 
 module.exports = router;
